0ef9e828dde2c5c49e20fd55e6d9a0386e712971
RizwanQubitars <rizwan@qubitars.com>
2025-04-15 20:39:42 +0500

    cors issue resolved

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   const port =process.env.PORT || 3000;
   await app.listen(port, ()=> console.log(`server is listening on port ${port}`));
 }
